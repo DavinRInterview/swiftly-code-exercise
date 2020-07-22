ef0523f2b9ab4ee1678e701cef272e5fbd742f93
DavinRInterview <68320463+DavinRInterview@users.noreply.github.com>
2020-07-22 16:47:37 -0700

    add license everywhere

--- app/src/main/java/com/swiftly/managerspecials/di/GlideModule.kt
+++
@@ -1,9 +0,0 @@
-package com.swiftly.managerspecials.di
-
-import com.bumptech.glide.annotation.GlideModule
-import dagger.Provides
-import javax.inject.Singleton
-
-@GlideModule
-class GlideModule {
-}
